891f527be58ff41c994acfe206d763d1c9a12432
protesilaos <info@protesilaos.com>
2016-07-22 11:04:29 +0300

    Correct error

--- sonho/hyperterm/index.js
+++ sonho/hyperterm/index.js
@@ -1,26 +1,26 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    foregroundColor: '#A8ACB6',
-    backgroundColor: '#282C36',
-    borderColor: '#383C46',
-    cursorColor: '#3C8CBB',
+    foregroundColor: '#BAA6A8',
+    backgroundColor: '#4A3638',
+    borderColor: '#5A4648',
+    cursorColor: '#D9A652',
     colors: [
-      '#282C36', // dark
-      '#A97552', // red
-      '#48B788', // green
-      '#3C8CBB', // blue
-      '#C789A6', // magenta
-      '#7A7DCA', // violet
-      '#55AFAF', // cyan
-      '#686C76', // gray dark
-      '#808188', // gray light
-      '#A97552', // red
-      '#48B788', // green
-      '#3C8CBB', // blue
-      '#C789A6', // magenta
-      '#7A7DCA', // violet
-      '#55AFAF', // cyan
-      '#EAEFF2', // light
+      '#4A3638', // dark
+      '#CD4679', // red
+      '#11BF8C', // green
+      '#DD619F', // magenta
+      '#5FB8B4', // cyan
+      '#A77CCF', // violet
+      '#479CB5', // blue
+      '#8A7678', // gray dark
+      '#8F8082', // gray light
+      '#CD4679', // red
+      '#11BF8C', // green
+      '#DD619F', // magenta
+      '#5FB8B4', // cyan
+      '#A77CCF', // violet
+      '#479CB5', // blue
+      '#FFF0F2', // light
     ],
     css: `
       ${config.css || ''}
@@ -31,10 +31,10 @@ exports.decorateConfig = (config) => {
         border: 0;
       }
       .tab_active {
-        background-color: #383C46;
+        background-color: #5A4648;
       }
       .tab_active::before {
-        border-bottom: 1px solid #3C8CBB;
+        border-bottom: 1px solid #D9A652;
       }
     `
   })
