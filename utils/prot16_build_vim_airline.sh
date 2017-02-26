28d86a1f86f8637df1f6a61a241d75a3c6423410
Protesilaos Stavrou <info@protesilaos.com>
2017-02-26 10:12:40 +0200

    Fix output path for vim plugins
    Files must be included in the appropriate directories in order to load properly.

--- utils/prot16_build_vim_airline.sh
+++ utils/prot16_build_vim_airline.sh
@@ -2,10 +2,10 @@
 for filename in $(ls ~/prot16-builder/db/schemes)
 do
   name=$(echo $filename | cut -f 1 -d '.')
-  mkdir -p /home/prot/prot16-vim-airline/
+  mkdir -p /home/prot/prot16-vim-airline/autoload/airline/themes
 
-  prot16-builder -s ${name} -t vim-airline -b light > /home/prot/prot16-vim-airline/${name}_light.vim
+  prot16-builder -s ${name} -t vim-airline -b light > /home/prot/prot16-vim-airline/autoload/airline/themes/${name}_light.vim
   tput setaf 1; echo "Preparing ${name} light for Vim airline"
-  prot16-builder -s ${name} -t vim-airline -b dark > /home/prot/prot16-vim-airline/${name}_dark.vim
+  prot16-builder -s ${name} -t vim-airline -b dark > /home/prot/prot16-vim-airline/autoload/airline/themes/${name}_dark.vim
   tput setaf 2; echo "Preparing ${name} dark for Vim airline"
 done
