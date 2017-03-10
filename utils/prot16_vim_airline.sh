8e34901746f05ed342f0eae0b7b42dbe64e622a0
Protesilaos Stavrou <info@protesilaos.com>
2017-03-10 13:07:31 +0200

    Update scripts

---
+++ utils/prot16_vim_airline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for filename in $(ls ~/prot16-builder/db/schemes)
+do
+  name=$(echo $filename | cut -f 1 -d '.')
+  mkdir -p /home/prot/prot16/${name}/vim/
+
+  prot16-builder -s ${name} -t vim-airline -b light > /home/prot/prot16/${name}/vim-airline/${name}_light.vim
+  tput setaf 1; echo "Preparing ${name} light for Vim airline"
+  prot16-builder -s ${name} -t vim-airline -b dark > /home/prot/prot16/${name}/vim-airline/${name}_dark.vim
+  tput setaf 1; echo "Preparing ${name} dark for Vim airline"
+done
