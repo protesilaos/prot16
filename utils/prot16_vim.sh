7586bab1e4cd8c5a388ad1da4496da973a6fb5f3
Protesilaos Stavrou <info@protesilaos.com>
2017-04-15 20:08:39 +0000

    Coorect path in mkdir

--- utils/prot16_vim.sh
+++ utils/prot16_vim.sh
@@ -2,7 +2,7 @@
 for filename in $(ls ~/prot16-builder/db/schemes)
 do
   name=$(echo $filename | cut -f 1 -d '.')
-  mkdir -p /home/prot/prot16-vim/colors
+  mkdir -p /home/prot/prot16/$name/vim/
 
   prot16-builder -s ${name} -t vim -b light > /home/prot/prot16/${name}/vim/${name}_light.vim
   tput setaf 2; echo "Preparing ${name} light for Vim"
