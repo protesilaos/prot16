ff77b51b676146bbc8d57854b46117e6c9ec7d22
Protesilaos Stavrou <info@protesilaos.com>
2017-04-15 20:06:50 +0000

    Add XFCE4 notifications

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ This is the master repository of all the items I have developed. Each directory 
   - a `README` with further instructions.
 - under `urxvt`:
   - a sample Xresources with the colour scheme (you need to copy/paste it into your `.Xresources`)
+- under `xfce4-notifications`:
+  - style sheets for the native XFCE4 notifications (add the file to `~/.themes/$theme-name/xfce-notify-4.0/gtk.css `)
 
 *NOTE: You can also get the theme you want for your favourite application by using the new [Prot16 Builder](https://github.com/protesilaos/prot16-builder).*
 
---
+++ utils/prot16_xfce4_notifications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for filename in $(ls ~/prot16-builder/db/schemes)
+do
+  name=$(echo $filename | cut -f 1 -d '.')
+  mkdir -p /home/prot/prot16/${name}/xfce4-notifications/
+
+  prot16-builder -s ${name} -t xfce4-notifications -b light > /home/prot/prot16/${name}/xfce4-notifications/${name}_light.css
+  tput setaf 1; echo "Preparing ${name} light for XFCE4 Notifications theme"
+  prot16-builder -s ${name} -t xfce4-notifications -b dark > /home/prot/prot16/${name}/xfce4-notifications/${name}_dark.css
+  tput setaf 2; echo "Preparing ${name} dark for XFCE4 Notifications theme"
+done
