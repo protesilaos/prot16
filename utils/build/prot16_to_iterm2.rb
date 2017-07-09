45347220744368158d4d35bf8b2073109e5e3134
Protesilaos Stavrou <info@protesilaos.com>
2017-07-09 09:37:50 +0300

    Minor adjusts to ruby script for iterm2

--- utils/build/prot16_to_iterm2.rb
+++ utils/build/prot16_to_iterm2.rb
@@ -143,9 +143,9 @@ FILE_HEADER = '<?xml version="1.0" encoding="UTF-8"?>
 FILE_FOOTER = '</dict>
 </plist>'.freeze
 
-scheme_names = %w(alto archaic bionis blau caprice cyprium equinox ficus flowerbed gaia magus nefelio neptune ocarina oliveira orionis overgrowth playa seabed sonho symbiosis vin termina hinterland)
+scheme_names = %w(alto archaic bionis blau caprice cyprium equinox ficus flowerbed fortuna gaia magus nefelio neptune ocarina oliveira orionis overgrowth playa seabed sonho symbiosis vin termina hinterland)
 scheme_names.each do |scheme_name|
-  input_file = "#{scheme_name}/general/#{scheme_name}-values.yml"
+  input_file = "#{scheme_name}/general/#{scheme_name}-palette.yml"
   scheme = YAML.load_file input_file
   scheme.keys.each { |k| scheme[k.delete("\$")] = scheme[k]}
   profile_name = scheme['scheme']
